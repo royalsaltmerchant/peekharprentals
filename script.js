4362be2792b9d479feee541d42396ab5583e53d7
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-29 12:42:18 -0700

    animated references

---
+++ script.js
@@ -0,0 +1,14 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var slide = document.getElementsByClassName('slide')
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > slide.length) {myIndex = 1}    
+  slide[myIndex-1].style.display = "inline-block";  
+  setTimeout(carousel, 9000); // Change image every 4 seconds
+}
